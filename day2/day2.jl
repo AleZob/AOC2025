a96f104a436ac55b0b0ef9f67f6fee0cc7342da0
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-09 22:57:39 +0100

    feat: day2part1 DONE!

---
+++ day2/day2.jl
@@ -0,0 +1,45 @@
+function parse_input(input_file)::Vector{UnitRange{Int64}}
+    s = ""
+    open(input_file, "r") do f
+        s = readline(f)
+    end
+    s = split(s, ",")
+    s = s .|> (x -> split(x, "-")) .|> x -> parse.(Int, x)
+    s = s .|> x -> range(x[1], x[2])
+    return s
+end
+
+function repeat(number)
+    return number * 10^(ndigits(number)) + number
+end
+
+function invalid_ID_1(input_file)
+    s = parse_input(input_file)
+    sum_out = 0
+    for range_s in s
+        @info range_s
+        start = range_s.start
+        stop = range_s.stop
+        startd = ndigits(start)
+        stopd = ndigits(stop)
+        ndig = 0
+        # No valid numbers to repeat (must be even)
+        isodd(startd) && isodd(stopd) && startd == stopd && continue
+
+        ndig = iseven(startd) ? startd / 2 : (startd + 1) / 2
+        number::Integer = 10^(ndig - 1)
+        repeated = 0
+        @info "Entering the loop"
+        while true
+            repeated = repeat(number)
+            repeated > stop && break
+            if repeated ∈ range_s
+                @info repeated
+                sum_out += repeated
+            end
+            number += 1
+        end
+        @info ("-"^10)
+    end
+    print(sum_out)
+end
