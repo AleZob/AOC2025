9ae8873e2ec09f45cda2025e2bda9eb41ddc4185
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-11 21:30:05 +0100

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Julia flake for the AOC2025";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default =
+      pkgs.mkShell
+      {
+        buildInputs = [
+          # Nix compiled julia (possibly cached)
+          (pkgs.julia-bin.withPackages
+            [
+              "ProgressMeter" # why not?
+
+              # lsp related:
+              "LanguageServer"
+              "SymbolServer"
+              "StaticLint"
+            ])
+        ];
+
+        shellHook = ''
+          echo "Julia-AOC2025 flake activated"
+        '';
+      };
+  };
+}
