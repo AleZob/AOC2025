b55b09c2ff01bd856b91c9664ab43bb47fc63a69
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-11 17:21:32 +0100

    feat: day2part2 basic template
    Just thinking

--- day2/day2.jl
+++ day2/day2.jl
@@ -43,3 +43,33 @@ function invalid_ID_1(input_file)
     end
     print(sum_out)
 end
+
+function invalid_ID_2(input_file)
+    s = parse_input(input_file)
+    sum_out = 0
+    for range_s in s
+        @info range_s
+        start = range_s.start
+        stop = range_s.stop
+        startd = ndigits(start)
+        stopd = ndigits(stop)
+        ndig = 0
+
+        ndig = iseven(startd) ? startd / 2 : (startd + 1) / 2
+        number::Integer = 10^(ndig - 1)
+        repeated = 0
+        # idea: start in another loop and increase digits, untill we have too much to repeat
+        @info "Entering the loop"
+        while true
+            repeated = repeat(number)
+            repeated > stop && break
+            if repeated ∈ range_s
+                @info repeated
+                sum_out += repeated
+            end
+            number += 1
+        end
+        @info ("-"^10)
+    end
+    print(sum_out)
+end
