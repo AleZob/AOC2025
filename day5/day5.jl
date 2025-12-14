da04f0efe0a56ceb61884220d00431da6d02505d
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-14 16:52:43 +0100

    feat!(day5part1): it finally ran for the entire thing.
    wrong answer tho, too low 803

--- day5/day5.jl
+++ day5/day5.jl
@@ -1,4 +1,6 @@
-using DataStructures
+# using DataStructures
+using ProgressMeter
+
 
 function parse_input(input_file)
     s = readlines(input_file)
@@ -13,26 +15,61 @@ function parse_input(input_file)
     return ranges, numbers
 end
 
-
-function push_range!(set::DisjointSets{Any}, range::UnitRange{Int})
-    repr = range.start
-    for x in range
-        if x ∉ set
-            push!(set, x)
-        end
-        union!(set, repr, x)
+function combine_ranges(a::UnitRange, b::UnitRange)
+    # ASSUME sort(a,b) <==> a,b
+    # that is a.start < b.start, if a.start == b.start => a.stop < b.stop
+    if b.start <= a.stop
+        return UnitRange(a.start, b.stop)
+    elseif a.stop == b.stop
+        return a
+    else
+        @warn "Should not happend"
     end
 end
 
+function combination(A)
+    out = zeros(size(A)) .|> Bool
+    R = CartesianIndices(A)
+    Ifirst, Ilast = first(R), last(R)
+    step = oneunit(Ifirst)
+    for I in R
+        s = intersect(A[I], A[min(I + step, Ilast)])
+        @show s A[I] A[min(I + step, Ilast)]
+        if s.start < s.stop
+            out[I] = 1
+
+        end
+    end
+    out
+end
+
+# function push_range!(set::DisjointSets{Any}, range::UnitRange{Int})
+#     repr = range.start
+#     for x in range
+#         if x ∉ set
+#             push!(set, x)
+#         end
+#         union!(set, repr, x)
+#     end
+# end
+
 
 function part_1(input_file)
-    ranges, numbers = parse_input(input_file)
-    @info "here"
-    sets = DisjointSets()
-    push_range!.(Ref(sets), ranges[begin:end])
-    @info "here"
-    fresh = in.(num, Ref(sets))
-    return reduce(+, fresh)
+    @showtime ranges, numbers = parse_input(input_file)
+    @show size(ranges)
+    @showtime sort!(ranges, by=first)
+    # combine_bollean = combination(ranges)
+    # combined_ranges = []
+    cont = 0
+    @info "Entering the loop"
+    @showprogress for i in numbers
+        found = searchsortedlast(ranges, i, by=first)
+        found == 0 && continue
+        if i ∈ ranges[found]
+            cont += 1
+        end
+    end
+    return cont
 end
 
 function part_2(input_file, num=12)
