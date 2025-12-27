1868babd29c3c7df98b946a0f85960384f480d52
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-27 20:52:31 +0100

    test(d6p2): test case
    - helped with the top edge case

--- day6/test.jl
+++ day6/test.jl
@@ -4,6 +4,6 @@ include("day.jl")
 @testset "Tests for Part 1" begin
     @test part_1("input_test.txt") == 4277556
 end
-# @testset "Tests for Part 2" begin
-# @test part_2("input_test.txt") == 14
-# end
+@testset "Tests for Part 2" begin
+    @test part_2("input_test.txt") == 3263827
+end
