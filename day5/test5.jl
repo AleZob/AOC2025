f6e3b9aa899f14c5afd76513819e5ef6a4c34ae0
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-27 18:40:09 +0100

    test(day5part2): added sanity checks for myself
    - Thanks to the reditor once again for an idea
    - Why do I need to be reminded about testing...

--- day5/test5.jl
+++ day5/test5.jl
@@ -3,3 +3,6 @@ include("day5.jl")
 
 # @test part_1("input_test.txt") == 3
 @test part_2("input_test.txt") == 14
+@test part_2("input_test1.txt") == 15
+@test part_2("input_test2.txt") == 16
+@test part_2("input_test3.txt") == 16
