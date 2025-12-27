cb8aac5b0b64f780aa8512029318df8f4d463c54
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-27 20:51:52 +0100

    feat(d6p2): done!
    - " " != ' ', one is string and the other is char, god damn
    - I love functional

--- day6/day.jl
+++ day6/day.jl
@@ -16,4 +16,30 @@ function part_1(input_file)
 end
 
 function part_2(input_file)
+    opers = Dict('*' => *, '+' => +)
+    input_tape = stack(readlines(input_file))
+    # oper = inputtape[:, end]
+    cont = 0
+    buff = []
+    oper = +
+    for row in reverse(eachrow(input_tape))
+        # @show row
+        # @show row[end] != ' '
+        # if empty
+        if join(row, "") == ' '^size(row)[1]
+            @show reduce(oper, buff)
+            cont += reduce(oper, buff)
+            buff = []
+            continue
+        end
+        # if we have op then this is last num
+        if row[end] != ' '
+            oper = opers[row[end]]
+        end
+        num = row[1:end-1] |> x -> join(x, "") |> x -> parse(Int, x)
+        append!(buff, num)
+    end
+    @show reduce(oper, buff)
+    cont += reduce(oper, buff) # forgot the edge case
+    cont
 end
