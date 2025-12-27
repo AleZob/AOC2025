7ce176a3f74ed268d87eaa8ee603bdb87734d30e
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-27 12:52:25 +0100

    test(day5part2): added test for part 2
    - It is CORRECT for test input
    - But tooo low for the big deal

--- day5/test5.jl
+++ day5/test5.jl
@@ -1,5 +1,5 @@
 using Test
 include("day5.jl")
 
-@test part_1("input_test.txt") == 3
-# @test part_2("input_test.txt") == 43
+# @test part_1("input_test.txt") == 3
+@test part_2("input_test.txt") == 14
