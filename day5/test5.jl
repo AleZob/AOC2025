c906b0542c1ef37d09aa407d73883bc2137ea4a7
Aleksei Zobnin <a.zobnin@student.tue.nl>
2025-12-27 19:15:24 +0100

    test(day5part2): more tests
    - finally found the overcounting yay

--- day5/test5.jl
+++ day5/test5.jl
@@ -1,8 +1,14 @@
 using Test
 include("day5.jl")
 
-# @test part_1("input_test.txt") == 3
-@test part_2("input_test.txt") == 14
-@test part_2("input_test1.txt") == 15
-@test part_2("input_test2.txt") == 16
-@test part_2("input_test3.txt") == 16
+# @testset "Tests for Part 1" begin
+#     @test part_1("input_test.txt") == 3
+# end
+@testset "Tests for Part 2" begin
+    @test part_2("input_test.txt") == 14
+    @test part_2("input_test1.txt") == 15
+    @test part_2("input_test2.txt") == 16
+    @test part_2("input_test3.txt") == 16
+    @test part_2("input_test4.txt") == 30
+    @test part_2("input_test5.txt") == 7
+end
